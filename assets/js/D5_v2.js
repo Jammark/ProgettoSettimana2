c29f6ef1d8e7b5043be70aa7b386d65c0de0bb3c
marcodinicola88 <marco.dinicola@yahoo.it>
2023-04-07 11:40:58 +0200

    commit numero 2

--- assets/js/D5_v2.js
+++ assets/js/D5_v2.js
@@ -11,17 +11,33 @@ REGOLE
 */
 const pets = ['dog', 'cat', 'hamster', 'redfish']
 
+for(var i in pets){
+  console.log('elemento numero ' + (i + 1) + ' : ' + pets[i]);
+}
+
 /* ESERCIZIO 2
     Scrivi del codice per ordinare alfabeticamente gli elementi dell'array "pets" e stampa l'array ordinato in console.
 */
 
+pets.sort();
+console.log(pets);
+
 /* ESERCIZIO 3
     Scrivi del codice per stampare nuovamente in console gli elementi dell'array "pets", questa volta in ordine invertito.
 */
+for(var i in pets){
+  let index = pets.length - i - 1;
+  console.log('elemento numero ' + (index + 1) + ' : ' + pets[index]);
+}
+
 
 /* ESERCIZIO 4
     Scrivi del codice per spostare il primo elemento dall'array "pets" in ultima posizione e stampa l'array in console.
 */
+
+var elemento = pets.shift();
+pets.push(elemento);
+console.log('pets : ' + pets);
 
 /* ESERCIZIO 5
     Dato il seguente array di oggetti, scrivi del codice per aggiungere ad ognuno di essi una proprietà "licensePlate" con valore a tua scelta e stampa l'array in console.
@@ -47,31 +63,139 @@ const cars = [
   },
 ]
 
+for (var i in cars){
+  let element = cars[i];
+  element["lycencePlate"] = 'LP' + i;
+}
+
+console.log(cars);
+
 /* ESERCIZIO 6
     Scrivi del codice per aggiungere un nuovo oggetto in ultima posizione nell'array "cars", rispettando la struttura degli altri elementi.
     Successivamente, rimuovi l'ultimo elemento della proprietà "trims" da ogni auto e stampa l'array in console.
 */
+
+let elementoNuovo = {
+  brand: 'Ferrari',
+    model: 'f16',
+    color: 'black',
+    trims: ['test1', 'test2'],
+    lycencePlate : 'LP4',
+}
+
+cars.push(elementoNuovo);
+
+cars.forEach(function(element){
+  //let element = cars[i];
+  element.trims.pop();
+});
+
+console.log("trim rimosso: ");
+console.log(cars);
 
 /* ESERCIZIO 7
     Scrivi del codice per salvare il primo elemento della proprietà "trims" di ogni auto nel nuovo array "justTrims", sotto definito e stampa in console l'array justTrims così popolato.
 */
 const justTrims = []
 
+cars.forEach(function(element){
+  //let element = cars[i];
+  let trim = element.trims[0];
+  justTrims.push(trim);
+});
+
+console.log("just trims");
+console.log(justTrims);
+
 /* ESERCIZIO 8
     Cicla l'array "cars" e costruisci un if/else statament per mostrare due diversi messaggi in console. Se la prima lettera della proprietà
     "color" ha valore "b", mostra in console "Fizz". Altrimenti, mostra in console "Buzz".
 */
 
+for(var i in cars){
+  let car = cars[i];
+  var letter;
+  
+  for (var c of car.color){
+    
+      letter = c;
+      break;
+    
+  }
+
+  if (letter == "b"){
+    console.log("Fizz");
+  }else{
+    console.log("Buzz");
+  }
+}
 /* ESERCIZIO 9
     Utilizza un ciclo while per stampare in console i valori del seguente array numerico fino al raggiungimento del numero 32 escluso.
 */
 const numericArray = [
   6, 90, 45, 75, 84, 98, 35, 74, 31, 2, 8, 23, 100, 32, 66, 313, 321, 105,
 ]
+var indiceCiclo = 0;
+while (indiceCiclo < numericArray.indexOf(32)){
+  console.log(numericArray[indiceCiclo]);
+  indiceCiclo++;
+}
 
 /* ESERCIZIO 10
     Partendo dall'array fornito e utilizzando un costrutto switch, genera un nuovo array composto dalle posizioni di ogni carattere all'interno
     dell'alfabeto italiano.
     es. [f, b, e] --> [6, 2, 5]
 */
-const charactersArray = [g, n, u, z, d]
+const charactersArray = ['g', 'n', 'u', 'z', 'd']
+
+var posizioni = charactersArray.map(posizione);
+console.log(posizioni);
+
+function posizione(valore){
+  switch(valore){
+    case 'a':
+      return 1;
+    case 'b':
+      return 2;
+    case 'c':
+      return 3;
+    case 'd':
+      return 4;
+    case 'e':
+      return 5;
+    case 'f':
+      return 6;
+    case 'g':
+      return 7;
+    case 'h':
+      return 8;
+    case 'i':
+      return 9;
+    case 'l':
+      return 10;
+    case 'm':
+      return 11;
+    case 'n':
+      return 12;
+    case 'o':
+      return 13;
+    case 'p':
+      return 14;
+    case 'q':
+      return 15;
+    case 'r':
+      return 16;
+    case 's':
+      return 17;
+    case 't':
+      return 18;
+    case 'u':
+      return 19;
+    case 'v':
+      return 20;
+    case 'z':
+      return 21;
+      default:
+        return -1;
+  }
+}
